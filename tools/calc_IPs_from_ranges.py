358ed35e405efecc90a8d9acb0256c063f11cdd9
Ron Cameron <rscameron@gmail.com>
2020-02-07 16:36:40 -0800

    Update calc_IPs_from_ranges.py

--- tools/calc_IPs_from_ranges.py
+++ tools/calc_IPs_from_ranges.py
@@ -2,7 +2,12 @@
 from ipaddress import *
 
 
-ip_range: 10.204.0.0-10.204.63.255
+ip_range = '10.204.0.0-10.204.63.255'
+ip_start = ip_range.split("-")[0]
+ip_end = ip_range.split("-")[1]
+ip_start_int = (int(IPv4Address(ip_start)))
+ip_end_int = (int(IPv4Address(ip_end)))
 
-
-print('The IP range is from  to .');
+print('\nThe dotted decimal IP range is from ' + ip_start + ' to ' + ip_end + '.')
+print('The integer IP range is from ' + str(ip_start_int) + ' to ' + str(ip_end_int) + '.')
+print('There are ' + str((ip_end_int + 1) - ip_start_int) + ' IP addresses in this range.\n')
